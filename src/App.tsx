6738d064fd41c3abeac72ab45c9463c4aa621d39
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-10-13 22:44:27 +0700

    feat: apply styles for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
-    <div>
-      <h1>Placeholder</h1>
+    <div className="flex flex-col justify-center items-center gap-5 min-h-screen mx-auto my-10">
+      <div className="flex gap-3">
+        <Button className="rounded-xl bg-black text-white hover:bg-gray-900">
+          Add
+        </Button>
+        <Button className="rounded-xl bg-black text-white hover:bg-gray-900">
+          Remove
+        </Button>
+        <Button className="rounded-xl bg-black text-white hover:bg-gray-900">
+          Checkout
+        </Button>
+      </div>
     </div>
   );
 }
